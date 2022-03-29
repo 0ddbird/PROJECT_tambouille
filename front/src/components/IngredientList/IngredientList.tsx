237b39d445b6108bdb4aaf8f3fbccc8b532e98f7
Giacomo Llinares <giacomo.llinares@gmail.com>
2022-03-29 15:43:22 +0200

    Added ability to consume and delete ingredients

--- front/src/components/IngredientList/IngredientList.tsx
+++ front/src/components/IngredientList/IngredientList.tsx
@@ -7,6 +7,8 @@ interface Iprops {
 
 type Ingredient = {
     name: string
+    isConsumed: boolean
+    id: string
 }
 
 const IngredientList: React.FC<Iprops> = () => {
@@ -16,20 +18,45 @@ const IngredientList: React.FC<Iprops> = () => {
 
   const handleNewIngredientKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'Enter') {
-      setIngredients(ingredients => [...ingredients, { name: newIngredientName }])
+      setIngredients(ingredients => [...ingredients, { name: newIngredientName, id: v4(), isConsumed: false }])
       setNewIngredientName('')
     }
   }
+
+  const handleConsumptionChange = (handledIngredient: Ingredient) => (e: ChangeEvent<HTMLInputElement>) => {
+    setIngredients(ingredients.map(ingredient => {
+      if (handledIngredient.id === ingredient.id) return { ...ingredient, isConsumed: e.target.checked }
+      return ingredient
+    }))
+  }
+
+  const handleClearClick = () => setIngredients(ingredients => ingredients.filter(ingredient => !ingredient.isConsumed))
+  console.log(ingredients)
+
   return (
       <div className='ingredient-list'>
           <div className='list'>
-              {ingredients.map(ingredient => <div key={v4()}>{ingredient.name}</div>)}
+              {
+
+              ingredients.map(ingredient => {
+                return (
+                    <div key={ingredient.id}>
+                        <input type='checkbox' checked={ingredient.isConsumed} onChange={handleConsumptionChange(ingredient)}/>
+                        {ingredient.name}
+                    </div>
+                )
+              }
+              )
+
+              }
+
           </div>
           <input
           value={newIngredientName}
           onChange={handlenewIngredientNameChange}
           onKeyDown ={handleNewIngredientKeyDown}
           />
+          <button onClick={handleClearClick}>Clear consumed</button>
       </div>
 
   )
